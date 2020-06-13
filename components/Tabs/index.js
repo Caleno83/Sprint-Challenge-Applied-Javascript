11e1f3a140152dd5063c39e09080d962ad3e1aa3
Jorge Jimenez <calenocam24@gmail.com>
2020-06-13 13:06:09 -0400

    finished Tabs component with Get request Data

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,40 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+//set up a GET request
+
+axios.
+
+get('https://lambda-times-backend.herokuapp.com/topics')
+
+.then((res) => {
+    console.log('This is the response: ', res);
+    const response = res.data.topics
+    response.forEach((e) => {
+        topicsContainer.appendChild(tabs(e));
+    })
+
+})
+
+.catch((err) => {
+    console.log('The error is : ', err);
+});
+
+//creating the tab structure 
+function tabs(arr) {
+
+    const tabDiv = document.createElement('div');
+
+    tabDiv.classList.add('tab');
+
+    tabDiv.textContent = arr;
+
+    return tabDiv;
+
+}
+
+//to check tab structure in the console
+console.log(tabs());
+
+const topicsContainer = document.querySelector('.topics');
